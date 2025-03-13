d21878a9352aa984cfa50613f610796813f63b33
Julia Rochatka <rochatka.julia.helena@gmail.com>
2025-03-13 13:43:13 +0100

    Dodaj pliki projektów.

---
+++ Kalkulator części - programowanie wizualne 2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Kalkulator_części___programowanie_wizualne_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(90, 131);
+            button1.Name = "button1";
+            button1.Size = new Size(225, 106);
+            button1.TabIndex = 0;
+            button1.Text = "Monitor";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(352, 131);
+            button2.Name = "button2";
+            button2.Size = new Size(225, 106);
+            button2.TabIndex = 1;
+            button2.Text = "Dysk";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(643, 131);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(198, 106);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(660, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 32);
+            label1.TabIndex = 3;
+            label1.Text = "c";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(981, 339);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private RichTextBox richTextBox1;
+        private Label label1;
+    }
+}
